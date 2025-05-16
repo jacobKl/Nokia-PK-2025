aceb085546865f25407950e848721a4c9a486686
Marcin Fortuna <marcin.fortuna@onet.eu>
2025-05-16 15:51:20 +0200

    refactor: remove redundancy

--- src/UE/Application/Sms/Sms.cpp
+++ src/UE/Application/Sms/Sms.cpp
@@ -1,15 +1,3 @@
 #include "Sms.hpp"
 
-namespace ue {
-
-Sms::Sms(std::string text, const common::PhoneNumber &from, const common::PhoneNumber &to,
-        const std::chrono::time_point<std::chrono::system_clock> &receiveTime)
-    : text(std::move(text))
-    , from(from)
-    , to(to)
-    , receiveTime(receiveTime)
-    , isRead(false)
-{
-}
-
-}
+namespace ue {}
