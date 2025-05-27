7252cb7a2edbaa973e7d8460bb7ca4e103e81708
mjkj09 <makejo09@gmail.com>
2025-05-27 02:58:35 +0200

    feat(tests): add ApplicationViewingSmsTestSuite

---
+++ src/UE/Tests/Application/TestSuites/ApplicationViewingSmsTestSuite.hpp
@@ -0,0 +1,37 @@
+#ifndef APPLICATION_VIEWING_SMS_TEST_SUITE_HPP
+#define APPLICATION_VIEWING_SMS_TEST_SUITE_HPP
+
+#include "ApplicationNotConnectedTestSuite.hpp"
+#include "Context.hpp"
+
+namespace ue
+{
+   class ApplicationViewingSmsTestSuite : public ApplicationNotConnectedTestSuite
+   {
+   protected:
+      ApplicationViewingSmsTestSuite()
+      {
+         using ::testing::_;
+         using ::testing::AnyNumber;
+         using ::testing::AtLeast;
+
+         /* konstruktor ConnectedState woła te metody – nie są istotą testów */
+         EXPECT_CALL(userPortMock, showConnected()).Times(AnyNumber());
+         EXPECT_CALL(userPortMock, doubleClickCallback(_)).Times(AnyNumber());
+         EXPECT_CALL(userPortMock, rejectCallback(_)).Times(AnyNumber());
+         EXPECT_CALL(userPortMock, smsSelectedCallback(_)).Times(AnyNumber());
+         EXPECT_CALL(userPortMock, showNewSmsIndicator(_)).Times(AnyNumber());
+
+         /* NotConnected  ➜  Connecting */
+         sendAttachReqOnSibRecieve();
+
+         /* Connecting  ➜  Connected */
+         EXPECT_CALL(timerPortMock, stopTimer()).Times(AnyNumber());
+         EXPECT_CALL(userPortMock, showConnected()).Times(AtLeast(1));
+         objectUnderTest.handleAttachAccept();
+      }
+
+      Context& context() { return objectUnderTest.getContext(); }
+   };
+}   // namespace ue
+#endif
